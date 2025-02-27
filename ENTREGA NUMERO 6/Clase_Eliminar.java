37323929c54003c9ac07e4ae1af5f6ca19ab069d
JosephMoralesC <80478064+JosephMoralesC@users.noreply.github.com>
2025-02-26 23:32:14 -0500

    Update Clase_Eliminar.java

--- ENTREGA NUMERO 6/Clase_Eliminar.java
+++ ENTREGA NUMERO 6/Clase_Eliminar.java
@@ -13,24 +13,25 @@ public class Eliminar {
             long nuevoNumero = EntradaNumero;
             long numero;
             boolean duplicado = false;
-            
+
             File url = new File("C:\\Users\\gabri\\OneDrive\\Documentos\\NetBeansProjects\\Trabajo6\\src\\main\\java\\Trabajo6\\Contacto.txt");
             if (!url.exists()) {
                 url.createNewFile();
             }
-            
+
             RandomAccessFile txt = new RandomAccessFile(url, "rw");
             while (txt.getFilePointer() < txt.length()) {
                 lineaTexto = txt.readLine();
                 String[] separador = lineaTexto.split("-");
                 nombre = separador[0];
                 numero = Long.parseLong(separador[1]);
+                
                 if (nombre.equals(nuevoNombre) || numero == nuevoNumero) {
                     duplicado = true;
                     break;
                 }
             }
-            
+
             if (duplicado) {
                 File urlTemp = new File("temp.txt");
                 RandomAccessFile txtTemp = new RandomAccessFile(urlTemp, "rw");
@@ -40,16 +41,17 @@ public class Eliminar {
                     lineaTexto = txt.readLine();
                     index = lineaTexto.indexOf('-');
                     nombre = lineaTexto.substring(0, index);
+                    
                     if (nombre.equals(nuevoNombre)) {
                         continue;
                     }
+                    
                     txtTemp.writeBytes(lineaTexto);
                     txtTemp.writeBytes(System.lineSeparator());
                 }
-                
+
                 txt.seek(0);
                 txtTemp.seek(0);
-                
                 while (txtTemp.getFilePointer() < txtTemp.length()) {
                     txt.writeBytes(txtTemp.readLine());
                     txt.writeBytes(System.lineSeparator());
@@ -64,11 +66,16 @@ public class Eliminar {
                 return resultado;
             } else {
                 txt.close();
-                resultado = "El nombre ingresado no existe.";
+                resultado = "El nombre ingresado no existe en la base de datos";
                 return resultado;
             }
-        } catch (IOException e) {
-            return "Error al eliminar el usuario: " + e.getMessage();
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+            return "";
+        } catch (NumberFormatException nef) {
+            System.out.println(nef);
+            return "";
         }
     }
 }
+
